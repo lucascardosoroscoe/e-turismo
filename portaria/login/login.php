11517d7e38aab28eb1796f8466b1e5614a4d2221
lucascardosoroscoe <lucascardosoroscoe@gmail.com>
2021-05-21 01:06:00 -0400

    Limpeza

--- portaria/login/login.php
+++
@@ -1,7 +0,0 @@
-<form id="dados" action="Dashboard.php" method="post">
-<?php
-
-include('../../produtor/includes/db_valores.php');
-$codigo = $_GET['codigo'];
-    header('Location: https://ingressozapp.com/portaria?codigo='.$codigo);
-?>
